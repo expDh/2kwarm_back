14c0eae38817cb37815749ab1a549670c91797b9
Kirill <kirya_gla@mail.ru>
2025-12-09 23:29:17 +0300

    first

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({  
     // origin: ['http://localhost:3001'],
-    origin: ['http://2kwarm.fun'],
+    origin: true,
     credentials: true,
     exposedHeaders: 'set-cookie',
   })
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
